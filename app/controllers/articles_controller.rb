6f586c615518a4e43288563f3a4613274e3226f7
Jacotri77 <slowerthanyou@gmail.com>
2018-02-15 00:29:22 +0000

    adding the first real message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
     end
   
     def article_params
-      params.require(:article).permit(:title, :description, :born_on)
+      params.require(:article).permit(:title, :description)
     end
   
   
