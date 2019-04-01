58ccb1116139a877fe5f1bb9a15ef13e986a0fe3
Jacotri77 <slowerthanyou@gmail.com>
2019-04-01 23:00:54 +0000

    fixing pagination for production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update]
   def index
-    @users = User.paginate(page: params[:page], per_page: 2)
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
     
   def new
