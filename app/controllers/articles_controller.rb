ebb563d034a20b19ff9a401bbd1be638ccc0e021
Jacotri77 <slowerthanyou@gmail.com>
2019-04-25 19:28:12 +0000

    fixed @articles.user issue in articles_controller..lost 3 hours of my life lol.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :description)
     end
     def require_same_user
-      if current_user != @article_user
+      if current_user != @article.user
         flash[:danger] = "You can only edit or delete your own articles!"
         redirect_to root_path
       end
