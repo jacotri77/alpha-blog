b2a5ba0ab958a3207bfd53795a5171c2d77122bb
Erik Jacobsen <erikjacobsen@Eriks-MacBook-Pro.local>
2020-02-03 10:17:45 -0800

    adding sentry project

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ end
 
 module AlphaBlog
   class Application < Rails::Application
+    Raven.configure do |config|
+      config.dsn = 'https://cf4476023b414d97b62e236a8c838c96:59d9121c5318432f9789d19e03ec0450@sentry.io/2214854'
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
