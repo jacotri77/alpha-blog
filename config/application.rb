1a9b0fbd6e4b50944bfffa9b4988ba0ddbd6a4bd
Erik Jacobsen <erikjacobsen@Eriks-MacBook-Pro.local>
2020-02-03 09:31:51 -0800

    adding sentry

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Raven.configure do |config|
+  config.dsn = 'https://b593cc8760de4843ac1a8be282bb0fac:6bafc5d7bd39459787dcaca0d171d3d0@sentry.io/2214292'
+end
+
 module AlphaBlog
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
