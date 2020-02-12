9ef8a670a9663b14c8a2d53f8e41de52af5bf7d7
Erik Jacobsen <erikjacobsen@Eriks-MacBook-Pro.local>
2020-02-12 13:31:14 -0800

    correcting user_id in application_controller

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Raven.configure do |config|
-  config.dsn = 'https://da560686c00f4fa2bee1f1dbe4d24224:92545fab3a0b4eecbd263285e55165cc@sentry.io/2472960'
-end
-
 module AlphaBlog
   class Application < Rails::Application
     
--- config/initializers/sentry.rb
+++
@@ -1,3 +0,0 @@
-Raven.configure do |config|
-  config.dsn = 'https://da560686c00f4fa2bee1f1dbe4d24224:92545fab3a0b4eecbd263285e55165cc@sentry.io/2472960'
-end
