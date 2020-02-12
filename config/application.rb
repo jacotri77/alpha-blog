1a93e3d39693ed7f83e119c642f67deb3cb3a9d9
Erik Jacobsen <erikjacobsen@Eriks-MacBook-Pro.local>
2020-02-12 14:55:13 -0800

    adding error controller

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(*Rails.groups)
 Raven.configure do |config|
   config.dsn = 'https://da560686c00f4fa2bee1f1dbe4d24224:92545fab3a0b4eecbd263285e55165cc@sentry.io/2472960'
   config.excluded_exceptions = []
-  config.attr = 'value'
 end
 
 module AlphaBlog
