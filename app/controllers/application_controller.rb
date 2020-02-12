44b80f526184621849b4a4c1362d41fa3a7bd0a9
Erik Jacobsen <erikjacobsen@Eriks-MacBook-Pro.local>
2020-02-12 11:14:33 -0800

    added before action to application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   helper_method :current_user, :logged_in?
-
+  before_action :set_raven_context
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
     
   end
   
-  before_action :set_raven_context
+  
 
   private
 
