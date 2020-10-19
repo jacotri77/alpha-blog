db9133d1a57500cb683bcddc94f968a5a15999d5
Erik Jacobsen <erikjacobsen@X-100022-LV.local>
2020-10-19 09:00:49 -0700

    committing the buttercms stuff

---
+++ config/initializers/buttercms.rb
@@ -0,0 +1,9 @@
+require 'buttercms-ruby'
+
+# If you added the Heroku Butter add-on, ENV["BUTTER_TOKEN"] will be defined.
+# Otherwise, grab your token at https://buttercms.com/profile/ and set it below
+# ButterCMS::api_token = ENV['a62dbe5b8be343cccc8a91ede2880ccf30adf261require 'buttercms-ruby'
+ButterCMS::api_token = 'a62dbe5b8be343cccc8a91ede2880ccf30adf261require';
+
+# Test mode can be used to setup a staging website for previewing content or for testing content during local development.
+# ButterCMS::test_mode = true
